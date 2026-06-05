2128a9bb47c00c25601ae5978072cafd55ffcf48
Vladislav Vasinev <vda5petr@rambler.ru>
2026-06-05 20:23:47 +0300

    feat: add validation for form

--- components/form/form.js
+++ components/form/form.js
@@ -3,8 +3,32 @@ const form = document.querySelector('form')
 
 if(form) {
 
+    function validate (formData) {
+
+        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+        const phoneRegex = /^\+?[1-9][0-9]{7,14}$/;
+
+        const formName = formData.get('name')
+        const formTel = formData.get('tel')
+        const formEmail = formData.get('email')
+
+        if(formName.length < 2) {
+            return false
+        }
+        if(!formEmail || !emailRegex.test(formEmail)) {
+            return false
+        }
+        if(!formTel || phoneRegex.test(formTel)) {
+            return false
+        }
+        else {
+            return true
+        }
+
+    }
+
     async function sendData(data) {
-        fetch('https:mysite.commmm', {
+        const response = await fetch('https:mysite.commmm', {
             method: "POST", 
             headers: {
                 'Content-Type': 'application/json'
@@ -23,12 +47,15 @@ if(form) {
 
         const formData = new FormData(form)
         
-        try {
-            await sendData(formData)    
-            form.insertAdjacentHTML('beforeend', '<p>Your data was successfully sent!</p>')
-        }
-        catch (error){
-            form.insertAdjacentHTML('beforeend', '<p>Unfortunately your data was not sent!</p>')
+        const validateValue = validate(formData)
+        if(validateValue) {
+            try {
+                await sendData(formData)    
+                form.insertAdjacentHTML('beforeend', '<p>Your data was successfully sent!</p>')
+            }
+            catch (error){
+                form.insertAdjacentHTML('beforeend', '<p>Unfortunately your data was not sent!</p>')
+            }
         }
     })
 
